bb2e0a07d3b31c30879f12945e0de7c749528784
MarionFoto <marionacp@gmail.com>
2017-05-12 01:42:36 -0300

    funciona la x

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,28 +1,23 @@
-
-document.getElementById("toggle").addEventListener("click", function(event){
+document.getElementById("toggle").addEventListener("click", function(event) {
     event.preventDefault();
-    document.getElementById("nav-header").classList.toggle("open");
-    document.getElementById("body").classList.toggle("overflow.hidden");
+    document.getElementById("header").classList.toggle("open");
+    document.getElementById("body").classList.toggle("overflow-hidden");
 });
 
-
-  
 var lastScrollTop = 0;
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
     var currentScroll = window.pageYOffset || document.body.scrollTop;
-    if (currentScroll > lastScrollTop){
-        console.log("down");
-}else{
-    document.getElementById("nav-header").classList.add("header-in");
-    document.getElementById("nav-header").classList.add("solid");
-    document.getElementById("nav-header").style.opacity = 1;
-    if (currentScroll<=3){
-        document.getElementById("nav-header").classList.remove("solid");
-        document.getElementById("nav-header").classList.remove("header-in");
-
+    if (currentScroll > lastScrollTop) {
+        document.getElementById("header").classList.remove("header-in");
+        document.getElementById("header").style.opacity = 0;
+    } else {
+        document.getElementById("header").classList.add("header-in");
+        document.getElementById("header").classList.add("solid");
+        document.getElementById("header").style.opacity = 1;
     }
-}
-
-});
-
-
+    if (currentScroll <= 3) {
+        document.getElementById("header").classList.remove("solid");
+        document.getElementById("header").classList.remove("header-in");
+    }
+    lastScrollTop = currentScroll; 
+}, false);
